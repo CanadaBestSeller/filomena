4705e2615b629d0a4c2658c39dd423b7915b8146
David Li <dli@etsy.com>
2023-01-26 22:30:48 -0500

    I added a panda to the title screen

--- components/landing/MainComponent.vue
+++ components/landing/MainComponent.vue
@@ -5,11 +5,18 @@
       {{ $t('lobby.roomDoesNotExist', [$route.params.invalidRoomId]) }}
     </b-alert>
 
-    <b-container class="mt-5">
-      <b-row><b-col><h1 class="text-center">{{ $t('landing.title') }}</h1></b-col></b-row>
-      <b-row><b-col><h5 class="text-center">{{ $t('landing.subtitle') }}</h5></b-col></b-row>
+    <b-container class="mt-4">
+      <b-row class="d-flex justify-content-center align-items-center">
+        <b-col cols="auto" class="d-flex justify-content-center px-2">
+          <b-img class="title-image mb-0" src="~/assets/images/panda-smile-face-only-3.png"/>
+        </b-col>
+        <b-col cols="auto">
+          <b-row><b-col><h1 class="m-0 text-center">{{ $t('landing.title') }}</h1></b-col></b-row>
+          <b-row><b-col><h5 class="m-0 text-center">{{ $t('landing.subtitle') }}</h5></b-col></b-row>
+        </b-col>
+      </b-row>
 
-      <b-row class="my-4" align-h="center" no-gutters>
+      <b-row class="my-3" align-h="center" no-gutters>
         <b-col cols="auto"><b-button class="shadow mx-1" size="lg" @click="joinParty()" variant="primary">{{ $t('landing.joinParty') }}</b-button></b-col>
         <b-col cols="auto"><b-button class="shadow mx-1" size="lg" @click="startParty()" variant="outline-success">{{ $t('landing.startParty') }}</b-button></b-col>
       </b-row>
@@ -33,3 +40,9 @@ export default {
   }
 }
 </script>
+
+<style>
+@media (max-width: 389px) { .title-image { max-height: 3rem } }
+@media (min-width: 390px) and (max-width: 768px) { .title-image { max-height: 6rem } }
+@media (min-width: 769px) { .title-image { max-height: 9rem } }
+</style>
