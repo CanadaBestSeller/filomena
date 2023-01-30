60149dffc8051d886e8f46c743b1e696482eefca
David Li <dli@etsy.com>
2023-01-29 20:59:59 -0500

    Panda face added to top link bar

--- components/room/LinkComponent.vue
+++ components/room/LinkComponent.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="shadow-sm bg-light pt-2 pb-1 d-flex flex-column justify-content-center room-link-div text-center">
-    <p style="font-family: 'Cera Round Pro Bold', sans-serif" class="my-0">{{ $t('lobby.inviteFriends') }}</p>
-    <b-link ref="roomLink" @click="copyLinkToClipboard()" class="room-link">
-      {{ roomLink }}
-    </b-link>
-    <p class="my-0" v-if="copied" style="font-family: 'Cera Round Pro Bold', sans-serif">{{ $t('general.copied') }}</p>
-    <p class="my-0" v-else @click="copyLinkToClipboard()">{{ $t('general.clickToCopyToClipboard') }}</p>
+  <div>
+    <b-img @click="goHome" class="d-none d-sm-block panda-face-img m-2" src="~/assets/images/panda-wink-2.png"/>
+    <div class="shadow-sm bg-light pt-2 pb-1 d-flex flex-column justify-content-center room-link-div text-center">
+      <p style="font-family: 'Cera Round Pro Bold', sans-serif" class="my-0 mx-auto">{{ $t('lobby.inviteFriends') }}</p>
+      <b-link ref="roomLink" @click="copyLinkToClipboard()" class="room-link mx-auto">
+        {{ roomLink }}
+      </b-link>
+      <p class="my-0 mx-auto" v-if="copied" style="font-family: 'Cera Round Pro Bold', sans-serif">{{ $t('general.copied') }}</p>
+      <p class="my-0 mx-auto width-au" v-else @click="copyLinkToClipboard()">{{ $t('general.clickToCopyToClipboard') }}</p>
+    </div>
   </div>
 </template>
 
@@ -35,7 +38,9 @@ export default {
         roomLinkRef.$el.style.opacity = 1
       }, 300)
       setTimeout(() => { this.copied = false }, 1000)
-    }
+    },
+
+    goHome() { this.$router.push('/') },
   },
 
   props: [
@@ -45,6 +50,15 @@ export default {
 </script>
 
 <style>
+.panda-face-img {
+  z-index: 2;
+  height: 4.5rem;
+  position: fixed;
+  top: 0;
+  left: 0;
+  cursor: pointer;
+}
+
 .room-link-div {
   position: fixed;
   width: 100vw;
