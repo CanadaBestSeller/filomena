448216ae6ae9a954d9c3a01444abd7faf6626b53
David Li <dli@etsy.com>
2023-02-03 19:31:44 -0500

    Fix: Dark Mode Popover

--- components/room/PlayerStatusPopover.vue
+++ components/room/PlayerStatusPopover.vue
@@ -6,7 +6,7 @@
 
     <b-popover class="shadow" target="popover-target" triggers="hover" placement="bottom">
       <b-list-group v-for="player in orderedPlayersWithTotalPoints" :key="player.uid" class="mt-2 mb-1 mx-0 text-center">
-        <b-list-group-item class="d-flex flex-row p-1 m-0 align-items-center border-0">
+        <b-list-group-item class="d-flex flex-row p-1 m-0 align-items-center border-0 bg-transparent">
           <h3 class="m-0">{{ player.emoji }}</h3>
           <span class="ml-2 mr-4">{{ player.name }}</span>
           <b-badge class="ml-auto" pill variant="primary">{{ player.totalPoints }}</b-badge>
